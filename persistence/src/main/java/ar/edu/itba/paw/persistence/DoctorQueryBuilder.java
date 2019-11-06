61b9063a0e38b3e009b960af5c2b56a2632c1704
Agustin Bossi <abossi@itba.edu.ar>
2019-11-06 19:45:21 -0300

    Revert "getFilteredDoctors back to before"
    This reverts commit 9bfdcb14c176d16dc4ac56a4edde01b6a8f3a5c1.

--- persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorQueryBuilder.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorQueryBuilder.java
@@ -5,20 +5,17 @@ public class DoctorQueryBuilder {
     private String query;
 
     public void buildQuery(String location, String specialty, String firstName, String lastName, String prepaid, int consultPrice){
-        StringBuilder query = new StringBuilder("select firstName, lastName, specialty, doctorLicense, phoneNumber, doctors.email," +
-                " id as clinicid, name, address, location, consultPrice " +
-                " from (((doctorclinics join doctors on doctorLicense = license) natural join (clinics join clinicPrepaids on clinicPrepaids.clinicid = clinics.id))" +
-                " join users on doctors.email = users.email) where ");
-        query.append( location!="" ? ("location = ? and ") : ("TRUE and ") );
-        query.append( specialty!="" ? ("specialty = ? and ") : ("TRUE and ") );
-        query.append( firstName!="" ? ("firstName = ? and ") : ("TRUE and ") );
-        query.append( lastName!="" ? ("lastName = ? and ") : ("TRUE and ") );
-        if(prepaid!=""){
-            query.append("clinicPrepaids.prepaid = ?;");
+        StringBuilder query = new StringBuilder("from DoctorClinic as doctorCli where ");
+        query.append( location != "" ? ("doctorCli.clinic.location.name = :location and ") : ("TRUE and ") );
+        query.append( specialty != "" ? ("doctorCli.doctor.specialty.name = :specialty and ") : ("TRUE and ") );
+        query.append( firstName != "" ? ("doctorCli.doctor.firstName = :firstName and ") : ("TRUE and ") );
+        query.append( lastName != "" ? ("doctorCli.doctor.lastName = :lastName ") : ("TRUE and ") );
+        if(prepaid != ""){
+            query.append("doctorCli.clinic.prepaids.prepaid = :prepaid"); //TODO query a list with a value?
         }else if(consultPrice > 0){
-            query.append("consultPrice <= ?;");
+            query.append("doctorCli.consultPrice <= :consultPrice");
         }else{
-            query.append("TRUE;");
+            query.append("TRUE");
         }
 
         this.query = query.toString();
