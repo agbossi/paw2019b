0224927767ebe8665d4cf134f56b00788e4bafcb
Agustin Bossi <abossi@itba.edu.ar>
2019-11-13 15:39:00 -0300

    doctorQueryBuilder successfully migrated

--- persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorQueryBuilder.java
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/DoctorQueryBuilder.java
@@ -5,19 +5,27 @@ public class DoctorQueryBuilder {
     private String query;
 
     public void buildQuery(String location, String specialty, String firstName, String lastName, String prepaid, int consultPrice){
-        StringBuilder query = new StringBuilder("from DoctorClinic as doctorCli where ");
-        query.append( location != "" ? ("doctorCli.clinic.location.name = :location and ") : ("TRUE and ") );
-        query.append( specialty != "" ? ("doctorCli.doctor.specialty.name = :specialty and ") : ("TRUE and ") );
-        query.append( firstName != "" ? ("doctorCli.doctor.firstName = :firstName and ") : ("TRUE and ") );
-        query.append( lastName != "" ? ("doctorCli.doctor.lastName = :lastName ") : ("TRUE and ") );
-        if(prepaid != ""){
-            query.append("doctorCli.clinic.prepaids.prepaid = :prepaid"); //TODO query a list with a value?
-        }else if(consultPrice > 0){
-            query.append("doctorCli.consultPrice <= :consultPrice");
-        }else{
-            query.append("TRUE");
+        StringBuilder query = new StringBuilder("select doctorCli from DoctorClinic as doctorCli inner join doctorCli.clinic.prepaids as p where ");
+        if(location != ""){
+            query.append("doctorCli.clinic.location.name = :location and ");
         }
-
+        if(specialty != ""){
+            query.append("doctorCli.doctor.specialty.name = :specialty and ");
+        }
+        if(firstName != ""){
+            query.append("doctorCli.doctor.firstName = :firstName and ");
+        }
+        if(lastName != ""){
+            query.append("doctorCli.doctor.lastName = :lastName ");
+        }
+        if(prepaid != ""){
+            query.append("p.prepaid.name = :prepaid"); //TODO query a list with a value?
+        }
+        else if(consultPrice > 0){
+            query.append("doctorCli.consultPrice <= :consultPrice");
+        }else {
+            query.append("1=1");
+        }
         this.query = query.toString();
     }
 
